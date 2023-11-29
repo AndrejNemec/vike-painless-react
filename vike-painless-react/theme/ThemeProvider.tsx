449f19b0dd0b8db615882bf7bd1112d8a84dc325
Andrej Nemec <andrejnemec007@gmail.com>
2023-11-29 13:28:39 +0100

    theme fix

--- vike-painless-react/theme/ThemeProvider.tsx
+++ vike-painless-react/theme/ThemeProvider.tsx
@@ -1,121 +1,102 @@
-import React, { ReactNode, createContext, useContext, useEffect, useMemo, useState, useRef } from 'react'
-import { ThemeScript, type ThemeWithAutoType, ThemeType } from "./ThemeScript.js"
+import React, { ReactNode, createContext, useContext, useEffect, useMemo, useState, useRef, useCallback } from 'react'
+import { ThemeScript, type ThemeWithAutoType, ThemeType } from './ThemeScript.js'
 import { getGlobalObject } from '../renderer/utils/getGlobalObject.js'
 
 export type ThemeProviderProps = {
-    defaultColorScheme?: ThemeWithAutoType
-    localStorageKey?: string
-    children?: ReactNode
+  defaultColorScheme?: ThemeWithAutoType
+  localStorageKey?: string
+  children?: ReactNode
 }
 
 const { Context: ThemeContext } = getGlobalObject('ThemeProvider.tsx', {
-    Context: createContext<{
-        setTheme: (theme: ThemeWithAutoType) => void
-        theme: ThemeWithAutoType
-        currentTheme: ThemeType
-    }>({} as any)
+  Context: createContext<{
+    setTheme: (theme: ThemeWithAutoType) => void
+    theme: ThemeWithAutoType
+    currentTheme: ThemeType
+  }>({} as any)
 })
 
 export const useColorScheme = () => {
-    return useContext(ThemeContext)
+  return useContext(ThemeContext)
 }
 
 const getThemeFromStorage = (localStorageKey: string): ThemeWithAutoType | null => {
-    if (typeof localStorage !== 'undefined') {
-        try {
-            const theme = localStorage.getItem(localStorageKey)
-            if (theme === 'light' || theme === 'dark' || theme === 'auto') {
-                return theme
-            }
-        } catch (e) { }
+  if (typeof localStorage !== 'undefined') {
+    try {
+      const theme = localStorage.getItem(localStorageKey)
+      if (theme === 'light' || theme === 'dark' || theme === 'auto') {
+        return theme
+      }
+    } catch (e) {
     }
-    return null
+  }
+  return null
 }
 
 const getSystemTheme = (): ThemeType => {
-    if (typeof window !== 'undefined') {
-        return window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light'
-    }
-    return 'light'
+  if (typeof window !== 'undefined') {
+    return window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light'
+  }
+  return 'light'
 }
 
 export const ThemeProvider = ({
-    defaultColorScheme = 'auto',
-    localStorageKey = 'app-color-scheme',
-    children
-}: ThemeProviderProps) => {
-    const [theme, setThemeState] = useState<ThemeWithAutoType>(defaultColorScheme)
-    const [currentTheme, setCurrentTheme] = useState<ThemeType>(theme === 'auto' ? 'light' : theme)
+                                defaultColorScheme = 'auto',
+                                localStorageKey = 'app-color-scheme',
+                                children
+                              }: ThemeProviderProps) => {
+  const [theme, setThemeState] = useState<ThemeWithAutoType>(defaultColorScheme)
+  const [currentTheme, setCurrentTheme] = useState<ThemeType>(theme === 'auto' ? 'light' : theme)
 
-    useEffect(() => {
-        const newTheme = getThemeFromStorage(localStorageKey) || defaultColorScheme
-        setThemeState(newTheme)
-        if (newTheme === 'auto') {
-            setCurrentTheme(getSystemTheme())
-        }
-    }, [])
+  const setTheme = useCallback((theme: ThemeWithAutoType, saveToLocalStorage: boolean = true) => {
+    setThemeState(theme)
+    setCurrentTheme(theme === 'auto' ? getSystemTheme() : theme)
+    localStorage.setItem(localStorageKey, theme)
+  }, [localStorageKey])
 
-    useEffect(() => {
-        if (theme === 'auto') {
-            setCurrentTheme(getSystemTheme())
-            return
-        }
-        setCurrentTheme(theme)
-    }, [theme])
+  useEffect(() => {
+    const newTheme = getThemeFromStorage(localStorageKey) || defaultColorScheme
+    setTheme(newTheme, false)
+  }, [setTheme])
 
-    useEffect(() => {
-        if (currentTheme === 'light') {
-            document.documentElement.classList.add('light')
-            document.documentElement.classList.remove('dark')
-        } else {
-            document.documentElement.classList.add('dark')
-            document.documentElement.classList.remove('light')
-        }
-        document.documentElement.style.setProperty('color-scheme', currentTheme)
-    }, [currentTheme])
+  useEffect(() => {
+    if (currentTheme === 'light') {
+      document.documentElement.classList.add('light')
+      document.documentElement.classList.remove('dark')
+    } else {
+      document.documentElement.classList.add('dark')
+      document.documentElement.classList.remove('light')
+    }
+    document.documentElement.style.setProperty('color-scheme', currentTheme)
+  }, [currentTheme])
 
-    useEffect(() => {
-        const handleStorage = (e: StorageEvent) => {
-            if (e.key !== localStorageKey) {
-                return
-            }
-            setThemeState(getThemeFromStorage(localStorageKey) || defaultColorScheme)
-        }
 
-        const handleMedia = () => {
-            if (theme === 'auto') {
-                setCurrentTheme(getSystemTheme())
-            }
-        }
-
-        window.addEventListener('storage', handleStorage)
-        
-        const darkModeMediaQuery = window.matchMedia('(prefers-color-scheme: dark)')
-        darkModeMediaQuery.addListener(handleMedia)
-
-        return () => {
-            window.removeEventListener('storage', handleStorage)
-            darkModeMediaQuery.removeListener(handleMedia)
-        }
-
-    }, [])
-
-    const setTheme = (theme: ThemeWithAutoType) => {
-        setThemeState(theme)
-        localStorage.setItem(localStorageKey, theme)
+  useEffect(() => {
+    const handleMedia = () => {
+      if (theme === 'auto') {
+        setCurrentTheme(getSystemTheme())
+      }
     }
 
-    return (
-        <ThemeContext.Provider value={{
-            theme,
-            currentTheme,
-            setTheme
-        }}>
-            <ThemeScript
-                defaultColorScheme={defaultColorScheme}
-                localStorageKey={localStorageKey}
-            />
-            {children}
-        </ThemeContext.Provider>
-    )
+    const darkModeMediaQuery = window.matchMedia('(prefers-color-scheme: dark)')
+    darkModeMediaQuery.addListener(handleMedia)
+
+    return () => {
+      darkModeMediaQuery.removeListener(handleMedia)
+    }
+  }, [theme])
+
+  return (
+    <ThemeContext.Provider value={{
+      theme,
+      currentTheme,
+      setTheme
+    }}>
+      <ThemeScript
+        defaultColorScheme={defaultColorScheme}
+        localStorageKey={localStorageKey}
+      />
+      {children}
+    </ThemeContext.Provider>
+  )
 }
